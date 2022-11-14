fe13cbb8e62b6789a1474e2225df5cb33df5f3a6
efe3535 <a.efe.akyazi@gmail.com>
2022-11-14 12:49:58 +0300

    aaa

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const http = require('http');const server = http.createServer((req, res) => {
         body += chunk.toString(); // convert Buffer to string
     });
     req.on('end', () => {
-//        console.log(body);
+        console.log(body);
 		fs.writeFile('test.txt', content,err => {});
 		res.end('ok');
     });
